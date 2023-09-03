625c428051bb66d2f4d57dcb9588e314d747fe13
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-09-03 05:34:07 +0200

    online

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,36 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
-import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+import fs from 'fs';
+import path from 'path';
+
+const dev = process.argv.includes('dev');
+
+const packageJsonPath = path.resolve('./package.json');
+const packageJsonData = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const repoFullName = packageJsonData.repository;
+const repoName = repoFullName.split('/')[1];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    "extensions": [".svelte", ...mdsvexConfig.extensions],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: [vitePreprocess({}), mdsvex(mdsvexConfig)],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess({}), mdsvex(mdsvexConfig)],
 
-    kit: {
+	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs'
+		}),
+		paths: {
+			base: dev ? '' : `/${repoName}`
+		}
 	}
 };
 
