93747449f2d7a640d0a5e3a5bef725da143f9a52
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-09-03 06:14:14 +0200

    add file fetch

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,13 @@
 export const load = async () => {
-	const documents = import.meta.glob('$lib/content/*.md', { eager: true });
-	const documentsPaths: string[] = [];
+	const documentsPaths = import.meta.glob('$lib/content/*.md', { eager: true });
+	const documents: string[] = [];
 
-	for (const path of Object.keys(documents)) {
-		documentsPaths.push(path);
+	for (const path of Object.keys(documentsPaths)) {
+		const fileName = path.split('/').at(-1);
+		const r = await fetch(
+			`https://raw.githubusercontent.com/bbtgnn/workshop-test/main/src/lib/content/${fileName}`
+		);
+		documents.push(await r.text());
 	}
-	return { documentsPaths };
+	return { documents };
 };
